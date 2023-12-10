acdd99e80a9663ad456742302675da136ee6aff2
Harvey Hsieh <zonyanofficial@gmail.com>
2023-12-11 06:36:49 +0800

    首頁拉皮

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo
-        title="LiveKit Meet | Conference app build with LiveKit Open Source"
+        title="NTUSTLIVE"
         titleTemplate="%s"
         defaultTitle="LiveKit Meet | Conference app build with LiveKit open source"
         description="LiveKit is an open source WebRTC project that gives you everything needed to build scalable and real-time audio and/or video experiences in your applications."
