f5fa60cfb8c51a8a6ecafaf0e4d169b09984d094
Harvey Hsieh <zonyanofficial@gmail.com>
2023-12-18 05:05:33 +0800

    Copilot把可能是NULL的user image...都弄好ㄌ

--- components/login-btn.tsx
+++ components/login-btn.tsx
@@ -11,8 +11,8 @@ export default function LoginBtn() {
     return (
 
       <>
-        <img className="rounded-full h-10 w-10" src={session.user.image} alt={session.user.name} />
-        <span>{session.user.name}</span>
+        <img className="rounded-full h-10 w-10" src={session.user?.image ?? ''} alt={session.user?.name ?? ''} />
+        <span>{session.user?.name ?? ''}</span>
         <button className="lk-button bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out" 
         onClick={() => signOut()}>Sign Out</button>
         
