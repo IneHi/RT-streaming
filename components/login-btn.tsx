8e2e2b2df907235b9965c997a22cd0bde384d506
Harvey Hsieh <zonyanofficial@gmail.com>
2023-12-18 06:42:36 +0800

    Sign in

--- components/login-btn.tsx
+++ components/login-btn.tsx
@@ -27,7 +27,7 @@ export default function LoginBtn() {
 
   
 
-    <button className="lk-button bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out" onClick={() => signIn()}>Sign in via Github</button>
+    <button className="lk-button bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out" onClick={() => signIn()}>Sign in to start !</button>
 
   )
 
